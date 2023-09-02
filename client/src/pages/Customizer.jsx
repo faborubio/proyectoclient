805ef8fbb6ed663efd38741dd703dcc385248214
faborubio <fabian.rubiocs@gmail.com>
2023-09-02 19:46:31 -0400

    add mejoras en view

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -18,6 +18,101 @@ import {
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState('')
+
+  const [activeFilterTab, setActiveFilerTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab contents depending on the activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />
+      case 'filepicker':
+        return <FilePicker 
+            file={file}
+            setFile={setFile}
+            readFile={readFile}
+          />
+      case 'aipicker':
+        return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />
+      default:
+        return null;
+    }
+  }
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert('Please enter a prompt');
+
+    try {
+      // call our backend to generate an AI image
+      setGeneratingImg(true);
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+      const data = await response.json();
+      console.log(data);
+      handleDecals(type,`data:image/png;base64,${data.photo}`)
+
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab('');
+    }
+  }
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case 'logoShirt':
+          state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case 'stylishShirt':
+        state.isFullTexture = !activeFilterTab[tabName];
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab("");
+      })  
+  }
+
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -26,8 +121,9 @@ const Customizer = () => {
             <div className='flex items-center min-h-screen'>
               <div className='editortabs-container tabs'>
                 {EditorTabs.map((tab) =>(
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}}/>
+                  <Tab key={tab.name} tab={tab} handleClick={() => setActiveEditorTab(tab.name)}/>
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
